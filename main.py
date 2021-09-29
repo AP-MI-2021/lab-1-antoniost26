94858771eb533f783e2025a80ab603c4d5ec543a
Antonio <antonio.stan@stud.ubbcluj.ro>
2021-09-29 09:06:06 +0300

    am terminat exercitiile

--- main.py
+++ main.py
@@ -2,12 +2,16 @@
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  flag=False
+  flag = False
   if n > 1:
-    for i in range(2, int((n/2)+1)):
-      if n % i:
-        flag=True
-  return flag
+    for i in range(2, n):
+      if n % i == 0:
+        flag = True
+        break
+  if flag == False:
+    return True
+  else:
+    return False
 
   
   
@@ -51,7 +55,7 @@ def get_cmmdc_v2(x, y):
   
 def main():
   lst = 1, 3, 6, 8
-  print(is_prime(13), get_product(lst), get_cmmdc_v1(6, 30), get_cmmdc_v2(6, 30))
+  print(is_prime(6), get_product(lst), get_cmmdc_v1(6, 30), get_cmmdc_v2(6, 30))
 
 if __name__ == '__main__':
   main()
